cfcce399a605964a0af050647d28d6065cda8edf
CrazyRabbit <crazyrabbit0@outlook.com>
2025-04-04 14:18:07 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
 export default {
   async fetch(request, env) {
-    const inputs = {
-      prompt: "cyberpunk cat",
-    };
+    const ERROR_RESPONSE = new Response("Something went wrong! 🥲");
 
+    const url = new URL(request.url);
+
+    if (!url.search) return ERROR_RESPONSE;
+    const params = new URLSearchParams(url.search);
+
+    if (!params.has('prompt')) return ERROR_RESPONSE;
+    const input = {
+      prompt: params.get('prompt'),
+    };
+    
     const response = await env.AI.run(
-      "@cf/stabilityai/stable-diffusion-xl-base-1.0",
-      inputs,
+      '@cf/black-forest-labs/flux-1-schnell',
+      input,
     );
 
-    return new Response(response, {
+    // Convert from base64 string
+    const binaryString = atob(response.image);
+    // Create byte representation
+    const img = Uint8Array.from(binaryString, (m) => m.codePointAt(0));
+    
+    return new Response(img, {
       headers: {
-        "content-type": "image/png",
+        'Content-Type': 'image/jpeg',
       },
     });
   },
